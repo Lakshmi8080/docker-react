47642898af0edaf5a6a495012de23d3173473998
Srikanth <sree7k7@gmail.com>
2020-02-23 07:23:33 +0100

    changes on typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jenkins is running <code>src/App.js</code> and save to reload.
+          Pipelines are working fine <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
